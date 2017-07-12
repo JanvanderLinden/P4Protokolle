2b55567ba8bf5b9a05df923a619b81d436797210
Jan van der Linden <uqdqa@student.kit.edu>
2017-07-13 00:17:57 +0200

    alles mal reingeschrieben

--- 07.10. Spez. Wärme/calc_area.py
+++ 07.10. Spez. Wärme/calc_area.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as sci
-
+import matplotlib
+matplotlib.rcParams.update({"font.size": 14})
+plt.rc("text", usetex = True)
 data = np.genfromtxt("SW_18", skip_header=2)
 T = data[:100,0]
 sw = data[:100,1]
-plt.plot(data[:,0],data[:,1])
-plt.show()
+#plt.plot(data[:,0],data[:,1])
+#plt.show()
 def cut_area(arr, min, max, arg):
 	i = 0
 	while T[i] < min:
@@ -27,25 +29,27 @@ f = lambda x,a,b: a*x+b
 
 popt, pcov = sci.curve_fit(f,bkgT,bkgsw)
 bkgfit = f(T,*popt)
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
 # plot 1: background fit
-plt.plot(T,sw,".")
-plt.plot(bkgT,bkgsw,".")
-plt.plot(T,bkgfit, "-")
-plt.show()
+plt.plot(T,sw,".", color = "blue", label = "signal peak")
+plt.plot(bkgT,bkgsw,".", color = "black", label = "background")
+plt.plot(T,bkgfit, "-", color = "red", lw = 2, label = "background fit")
+plt.legend()
+plt.xlim([80,100])
+plt.ylim([30,50])
 
 
 a_err = np.sqrt(pcov[0][0])
 b_err = np.sqrt(pcov[1][1])
 print(popt[0],a_err)
 print(popt[1],b_err)
-
 df = lambda x,a,b,da,db: np.sqrt( (x*da)**2 + db**2)
 
 signal = sw - bkgfit
 sig_err = [df(xx,popt[0],popt[1],a_err,b_err) for xx in sw]
 
-plt.clf()
 sigT = cut_area(T, 85,95,"in")
 sigsw = cut_area(signal,85,95,"in")
 sigsw_err = cut_area(sig_err,85,95,"in")
@@ -58,11 +62,25 @@ for i in range(len(sigT)):
 		sq_err += (sigsw_err[i]*(sigT[i+1]-sigT[i-1])/2.)**2	
 	except:
 		continue
+
+ax.text(0.03,0.97,
+    "fitted background function:\n$%s$\n$m = %.3f \\pm %.3f$\n$b = %.3f \\pm %.3f$\ncalculated signal area:\n$A = %.3f \\pm %.3f$"%(
+    "c(t) = m\\cdot T + b",
+	popt[0], a_err,
+	popt[1], b_err,
+	area, np.sqrt(sq_err)),
+	bbox = {"facecolor": "white"}, va = "top", ha = "left",
+	fontsize = 14, transform = ax.transAxes)
+plt.xlabel("Temperature $T$ / $\\textrm{K}$", fontsize = 16)
+plt.ylabel("specific heat $C$ / $\\frac{\\textrm{J}}{\\textrm{K} \\cdot \\textrm{mol}}$", fontsize = 16)
+plt.grid()
+plt.show()
 # plot2: signal region
+'''
 plt.errorbar(sigT, sigsw,yerr = sigsw_err, fmt = "o")
 plt.title("{:.3f}, {:.3f}".format(area, np.sqrt(sq_err)))
 plt.show()
 
 
 
-
+'''
--- 07.10. Spez. Wärme/fit2.py
+++ 07.10. Spez. Wärme/fit2.py
@@ -2,8 +2,10 @@ import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import matplotlib
+matplotlib.rcParams.update({"font.size": 14})
 
-data = np.genfromtxt("SW_test.txt", skip_header=2)
+data = np.genfromtxt("SW_18", skip_header=2)
 T = data[400:,0]
 sw = data[400:,1]
 
@@ -22,21 +24,24 @@ t = [ (Ti - TN)/TN for Ti in T]
 bkg = [ 25.*ti + 16. for ti in t]
 
 signal = sw-bkg
+'''
 plt.plot(t,signal)
 plt.show()
 plt.clf()
+'''
 rev_neg_t = t[:max_index]
 neg_t = [-ti for ti in rev_neg_t]
 pos_t = t[max_index:]
 pos_sw = signal[max_index:]
 neg_sw = signal[:max_index]
 
-cut = 3
-neg_t = neg_t[:-cut]
-rev_neg_t = rev_neg_t[:-cut]
-pos_t = pos_t[cut:]
-neg_sw = neg_sw[:-cut]
-pos_sw = pos_sw[cut:]
+cutp = 4
+cutn = 4
+neg_t = neg_t[:-cutn]
+rev_neg_t = rev_neg_t[:-cutn]
+pos_t = pos_t[cutp:]
+neg_sw = neg_sw[:-cutn]
+pos_sw = pos_sw[cutp:]
 
 log_nt = np.log10(neg_t)
 log_pt = np.log10(pos_t)
@@ -52,25 +57,49 @@ popt_n, pcov_n = curve_fit(f,log_nt,log_nsw)
 #fit positives
 popt_p, pcov_p = curve_fit(f,log_pt,log_psw)
 
-fit_n = f(log_nt,*popt_n)
-fit_p = f(log_pt,*popt_p)
+fit_tp = np.linspace(0.001,0.12,1000)
+fit_tn = np.linspace(-0.12,-0.001,1000)
+fit_tn_rev = [-ti for ti in fit_tn]
+fit_tpl = np.log10(fit_tp)
+fit_tnl = np.log10(fit_tn_rev)
 
+
+fit_n = f(fit_tnl,*popt_n)
+fit_p = f(fit_tpl,*popt_p)
+print(popt_n)
+print(popt_p)
+'''
 plt.plot(log_nt,log_nsw, ".", label = "neg")
 plt.plot(log_pt,log_psw, ".",label = "pos")
-plt.plot(log_nt,fit_n)
-plt.plot(log_pt,fit_p)
+plt.plot(fit_tnl,fit_n)
+plt.plot(fit_tpl,fit_p)
 plt.legend()
 plt.show()
 plt.clf()
-
+'''
 # rescale
 re_fit_n = 10**fit_n
 re_fit_p = 10**fit_p
-plt.plot(t,signal)
-plt.plot(rev_neg_t, neg_sw, "o", color = "black")
-plt.plot(pos_t, pos_sw, "o", color = "black")
-plt.plot(rev_neg_t, re_fit_n, lw = 1.5, color = "red")
-plt.plot(pos_t, re_fit_p, lw = 1.5, color = "red")
+
+plt.rc("text", usetex = True)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.xlabel("$t = \\frac{T - T_N}{T_N}$", fontsize = 16)
+plt.ylabel("reduced specific heat / $\\frac{\\textrm{J}}{\\textrm{K} \\cdot \\textrm{mol}}$", fontsize = 16)
+plt.plot(t,signal, ".", color = "black", label = "data")
+plt.plot(fit_tn, re_fit_n, lw = 1.5, color = "red", label = "fit for negative t")
+plt.plot(fit_tp, re_fit_p, lw = 1.5, color = "blue", label = "fit for positive t")
+
+ax.text(0.03,0.97,
+	"fitted function:\n$%s$\nnegative times:\n$A^- = %.3f \\pm %.3f$\n$\\alpha = %.3f \\pm %.3f$\npositive times:\n$A^+ = %.3f \\pm %.3f$\n$\\alpha = %.3f \\pm %.3f$"%(
+	"\\ln(c(t)) = \\ln\\left( \\frac{A^\\pm}{\\alpha}\\right) - \\alpha \\ln(t)",
+	popt_n[0], np.sqrt(pcov_n[0][0]), popt_n[1], np.sqrt(pcov_n[1][1]),
+	popt_p[0], np.sqrt(pcov_p[0][0]), popt_p[1], np.sqrt(pcov_p[1][1])),
+	bbox = {"facecolor": "white"}, va = "top", ha = "left",
+	fontsize = 14, transform = ax.transAxes)
+
+plt.grid()
+plt.legend()
 plt.plot()
 '''
 plt.plot(t,signal)
